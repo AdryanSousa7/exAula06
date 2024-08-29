1c4354fd5d59a7a87e88d90c626b730a74e32fb2
AdryanSousa7 <adryansousa063@gmail.com>
2024-08-29 15:55:30 -0300

    Add files via upload

---
+++ src/entidade/Pessoa.java
@@ -0,0 +1,17 @@
+package entidade;
+
+public class Pessoa {
+	
+	private String nome;
+	private double salario;
+	private int idade;
+	
+	public Pessoa(String nome, double salario, int idade) {
+		this.nome = nome;
+		this.salario = salario;
+		this.idade = idade;
+	}
+	
+	
+
+}
---
+++ src/principal/Main.java
@@ -0,0 +1,13 @@
+package principal;
+
+import entidade.Pessoa;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Pessoa V = new Pessoa("Adryan", 20,20);
+		
+		System.out.println(V);
+	}
+}
